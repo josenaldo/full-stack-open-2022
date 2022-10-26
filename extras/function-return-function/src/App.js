689959cca54caa85241497bb0a2c0fd0b8d544fe
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-10-26 17:02:28 -0300

    Testing function that returning functions as handler generators

---
+++ extras/function-return-function/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+function App() {
+    const [message, setMessage] = useState('')
+
+    const people = [
+        { name: 'John', age: 20 },
+        { name: 'Bob', age: 30 },
+        { name: 'Alice', age: 40 },
+        { name: 'Jane', age: 50 },
+    ]
+
+    const hello = (person) => {
+        const handler = () => {
+            setMessage('Hello ' + person.name)
+        }
+        return handler
+    }
+
+    return (
+        <div>
+            <h1>Function Return Function</h1>
+            {people.map((person) => {
+                return (
+                    <div key={person.name}>
+                        <button onClick={hello(person)}>
+                            Say Hello to {person.name}
+                        </button>
+                    </div>
+                )
+            })}
+
+            <h2>{message}</h2>
+        </div>
+    )
+}
+
+export default App
