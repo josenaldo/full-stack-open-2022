e56e9601e6f7acb94fda39ee915ead0199e7b3c8
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-10-31 20:17:24 -0300

    2.1: Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,63 +1,34 @@
 import './App.css'
+import Course from './components/Course'
 
 const App = () => {
     const course = {
+        id: 1,
         name: 'Half Stack application development',
         parts: [
             {
                 name: 'Fundamentals of React',
                 exercises: 10,
+                id: 1,
             },
             {
                 name: 'Using props to pass data',
                 exercises: 7,
+                id: 2,
             },
             {
                 name: 'State of a component',
                 exercises: 14,
+                id: 3,
             },
         ],
     }
 
     return (
         <div className="App">
-            <Header course={course} />
-
-            <Content course={course} />
-
-            <Total course={course} />
+            <Course course={course} />
         </div>
     )
-}
-
-const Header = (props) => {
-    return <h1>{props.course.name}</h1>
-}
-
-const Part = (props) => {
-    return (
-        <p>
-            {props.part.name} {props.part.exercises}
-        </p>
-    )
-}
-
-const Content = (props) => {
-    return (
-        <div>
-            {props.course.parts.map((part) => (
-                <Part part={part} key={part.name} />
-            ))}
-        </div>
-    )
-}
-
-const Total = (props) => {
-    const total = props.course.parts.reduce((sum, part) => {
-        return sum + part.exercises
-    }, 0)
-
-    return <p>{`Number of exercises ${total}`}</p>
 }
 
 export default App
