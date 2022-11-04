5d73b92c0bcee6ddeef89b97420f57d6f22210fd
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-04 17:37:02 -0300

    :sparkles: feat(part2notes): Adição de evento de adição de nota

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,7 +1,15 @@
-import './App.css'
+import { useState } from 'react'
 import Note from './components/Note'
+import './App.css'
 
-const App = ({ notes }) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+    }
+
     return (
         <div className="App">
             <h1>Notes</h1>
@@ -10,6 +18,10 @@ const App = ({ notes }) => {
                     <Note key={note.id} note={note} />
                 ))}
             </ul>
+            <form onSubmit={addNote}>
+                <input />
+                <button type="submit">save</button>
+            </form>
         </div>
     )
 }
