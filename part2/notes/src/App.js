a1e1864b1c80c32cceaad121b850d4c8ab160f5b
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-04 22:18:52 -0300

    :sparkles: feat(part2/notes): Add new note
    Create event hadlers to add a new note

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -4,10 +4,24 @@ import './App.css'
 
 const App = (props) => {
     const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState('a new note...')
 
     const addNote = (event) => {
         event.preventDefault()
-        console.log('button clicked', event.target)
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+            id: notes.length + 1,
+        }
+
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
     }
 
     return (
@@ -19,7 +33,7 @@ const App = (props) => {
                 ))}
             </ul>
             <form onSubmit={addNote}>
-                <input />
+                <input value={newNote} onChange={handleNoteChange} />
                 <button type="submit">save</button>
             </form>
         </div>
