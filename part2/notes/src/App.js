b10b0e47ab3ae593867441edd1ca2ef0341d6977
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-08 14:29:20 -0300

    :sparkles: feat: Add noteService
    Add a service component to handle with all comunication with the server

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import Note from './components/Note'
+import Note from 'components/Note'
+import noteService from 'services/notes'
+
 import './App.css'
 
 const App = (props) => {
@@ -11,6 +13,10 @@ const App = (props) => {
 
     const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
+    }
+
     const addNote = (event) => {
         event.preventDefault()
         const noteObject = {
@@ -19,31 +25,24 @@ const App = (props) => {
             important: Math.random() < 0.5,
         }
 
-        axios
-            .post('http://localhost:3001/notes', noteObject)
-            .then((response) => {
-                const createdNote = response.data
-                setNotes(notes.concat(createdNote))
-                setNewNote('')
-            })
-    }
-
-    const handleNoteChange = (event) => {
-        setNewNote(event.target.value)
+        noteService.create(noteObject).then((response) => {
+            const createdNote = response.data
+            setNotes(notes.concat(createdNote))
+            setNewNote('')
+        })
     }
 
     const toggleImportanceOf = (id) => {
-        const url = `http://localhost:3001/notes/${id}`
         const note = notes.find((n) => n.id === id)
         const changeNote = { ...note, important: !note.important }
 
-        axios.put(url, changeNote).then((response) => {
+        noteService.update(id, changeNote).then((response) => {
             setNotes(notes.map((n) => (n.id !== id ? n : response.data)))
         })
     }
 
     const hook = () => {
-        axios.get('http://localhost:3001/notes').then((response) => {
+        noteService.getAll().then((response) => {
             setNotes(response.data)
         })
     }
