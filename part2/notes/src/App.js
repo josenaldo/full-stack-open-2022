eccc5cd5a6f7703d482962a48b398f8141514f9d
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-09 22:22:31 -0300

    :sparkles: feat: Add error message
    Add a notification component, to show error messages

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import Note from 'components/Note'
+import Notification from 'components/Notification'
 import noteService from 'services/notes'
 
 import './App.css'
@@ -10,6 +11,7 @@ const App = (props) => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('a new note...')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('some error happened...')
 
     const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
@@ -33,9 +35,14 @@ const App = (props) => {
                 setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)))
             })
             .catch((error) => {
-                alert(
-                    `the note '${note.content}' was already deleted from server`
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
                 )
+
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
+
                 setNotes(notes.filter((n) => n.id !== id))
             })
     }
@@ -58,6 +65,7 @@ const App = (props) => {
     return (
         <div className="App">
             <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
