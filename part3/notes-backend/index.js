7a49af924d805584cfc907148e92883eeb9eea58
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-23 19:23:39 -0300

    :sparkles: feat: Notes validation

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -42,22 +42,20 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ error: 'content missing' })
-    }
+app.post('/api/notes', (request, response, next) => {
+    const { content, important } = request.body
 
     const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        data: new Date(),
+        content: content,
+        important: important || false,
+        date: new Date(),
     })
 
-    note.save().then((savedNote) => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then((savedNote) => {
+            response.json(savedNote)
+        })
+        .catch((error) => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -86,14 +84,18 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
     const note = {
-        content: body.content,
-        important: body.important,
+        content,
+        important,
     }
 
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id, note, {
+        new: true,
+        runValidators: true,
+        context: 'query',
+    })
         .then((updatedNote) => {
             response.json(updatedNote)
         })
@@ -117,6 +119,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
