6d1a7a64e947485df750ae1b2eead126b4a24835
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-23 15:13:38 -0300

    :sparkles: feat: Error handling

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -15,24 +15,22 @@ morgan.token('body', (request, response) => {
     }
 })
 
-const Note = require('./models/note')
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(
-    morgan(`---
+const morganTemplate = `---
 PATH: :url
 METHOD: :method
 STATUS: :status
 RES TIME: :response-time ms
 RES LENGTH: :res[content-length]
 BODY: :body
----`)
-)
+---`
 
+const Note = require('./models/note')
+
+const app = express()
 app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+app.use(morgan(morganTemplate))
 
 /**
  * ROUTES
@@ -62,32 +60,68 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     const id = request.params.id
 
     Note.findById(id)
         .then((note) => {
-            response.json(note)
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
         })
-        .catch((err) => {
-            console.log('🔴', err)
-            response.status(404).end()
-        })
+        .catch((err) => next(err))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
     const id = request.params.id
 
-    Note.deleteOne({ _id: id })
+    Note.findByIdAndRemove(id)
         .then((result) => {
             console.log('🟢', result)
             response.status(204).end()
         })
-        .catch((err) => {
-            console.log('🔴', err)
-            response.status(500).end()
-        })
+        .catch((err) => next(err))
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then((updatedNote) => {
+            response.json(updatedNote)
+        })
+        .catch((err) => next(err))
+})
+
+/**
+ * ERROR HANDLING
+ */
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+// handler of requests with result to errors
+const errorHandler = (error, request, response, next) => {
+    console.error('🔴', error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 /**
  * SERVER
